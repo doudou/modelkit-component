c5a1189f581549879cfebbadf9679cd3fe494531
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-11-17 10:17:01 -0200

    add test/suite.rb to keep track of which tests are supposed to pass

---
+++ test/suite.rb
@@ -0,0 +1,4 @@
+require 'modelkit/component/configuration_object_test'
+require 'modelkit/component/port_test'
+require 'modelkit/component/output_port_test'
+require 'modelkit/component/input_port_test'
